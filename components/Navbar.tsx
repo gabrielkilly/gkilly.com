1f45b1b59c7e9303c6231cc55e1420c4b7369107
Gabriel Killy <gabykilly@gmail.com>
2023-11-15 19:02:58 -0600

    lint issue fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export enum NavLinks {
   projects = "/projects",
   resume = "https://mega.nz/file/f8clERpT#1ot9ZkumrBCrtUT5Wr6cqMFiC5SmAZocDqj4o5EUj5w"
@@ -8,7 +10,7 @@ export default function Navbar() {
     return (
       <nav className="w-full bg-orange-2 py-2 flex justify-center">
         <div className="max-w-screen-xl w-10/12 flex justify-between">
-            <a className="nav" href="/">gabriel.killy</a>
+            <Link className="nav" href="/">gabriel.killy</Link>
 
             <div className="flex gap-7">
                 {/* <a className="nav" href={NavLinks.projects}>projects</a> */}
